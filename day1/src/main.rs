69cb22d1aef041e73a55d827456bc19a65925fa2
Giulia Ye <yg97.cs@gmail.com>
2022-12-01 12:19:27 +0100

    day1: keep both answers

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -11,15 +11,18 @@ fn main() {
         .into_string()
         .unwrap();
     let file_path = format!("{}/input.txt", current_dir);
-    println!("In file {}", file_path);
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     let all_reindeers_calories = calories_calculator(contents);
-    let greatest_calories = sum_of_greatest_numbers(all_reindeers_calories, 3);
+
+    println!(
+        "The reindeer with the greatest calories has: {:?}",
+        sum_of_greatest_numbers(all_reindeers_calories.clone(), 1)
+    );
 
     println!(
         "The top 3 reindeers with the greatest calories has: {:?}",
-        greatest_calories
+        sum_of_greatest_numbers(all_reindeers_calories, 3)
     )
 }
