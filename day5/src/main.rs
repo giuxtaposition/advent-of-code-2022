5aa7d172090d785b3b7b06f494dc452160b04564
Giulia Ye <yg97.cs@gmail.com>
2022-12-05 16:56:14 +0100

    refactor

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, str::FromStr};
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
@@ -8,24 +8,46 @@ fn main() {
 }
 
 fn top_crates_calculator(contents: String, keep_order: bool) -> String {
-    let lines = contents.lines().collect::<Vec<&str>>();
+    let mut parts = contents.split("\n\n");
 
-    let position = lines.iter().position(|line| line.is_empty());
+    let (schema, instructions): (Vec<&str>, Vec<&str>) = (
+        parts.next().unwrap().lines().collect(),
+        parts.next().unwrap().lines().collect(),
+    );
 
-    let (schema, instructions) = lines.split_at(position.unwrap());
-
-    let mut parsed_schema = parse_schema(schema.to_vec());
+    let mut stacks = parse_schema(schema);
 
     for instruction in instructions {
-        if !instruction.is_empty() {
-            parsed_schema = move_crate(instruction, parsed_schema, keep_order);
-        }
+        stacks = move_crate(instruction, stacks, keep_order);
     }
 
-    top_crates(parsed_schema)
+    top_crates(stacks)
 }
 
-fn top_crates<'a>(stacks: Vec<Vec<char>>) -> String {
+struct Instruction {
+    from: usize,
+    to: usize,
+    amount: usize,
+}
+
+impl FromStr for Instruction {
+    type Err = String;
+
+    fn from_str(instruction: &str) -> Result<Self, Self::Err> {
+        let container: Vec<usize> = instruction
+            .split_whitespace()
+            .filter_map(|s| s.parse::<usize>().ok())
+            .collect();
+
+        Ok(Self {
+            amount: container[0],
+            from: container[1],
+            to: container[2],
+        })
+    }
+}
+
+fn top_crates(stacks: Vec<Vec<char>>) -> String {
     stacks
         .iter()
         .map(|stack| {
@@ -62,35 +84,28 @@ fn parse_schema(mut input: Vec<&str>) -> Vec<Vec<char>> {
 }
 
 fn move_crate(
-    instructions_string: &str,
+    instruction_string: &str,
     mut stacks: Vec<Vec<char>>,
     keep_order: bool,
 ) -> Vec<Vec<char>> {
-    let instructions = parse_instruction(instructions_string);
+    let instruction = instruction_string.parse::<Instruction>().unwrap();
 
-    let n_cargo_to_keep = stacks[instructions[1] - 1].len() - instructions[0];
+    let n_cargo_to_keep = stacks[instruction.from - 1].len() - instruction.amount;
 
-    let mut stack_to_update = stacks[instructions[1] - 1].split_off(n_cargo_to_keep);
+    let mut stack_to_update = stacks[instruction.from - 1].split_off(n_cargo_to_keep);
 
     if !keep_order {
         stack_to_update.reverse()
     }
 
     stack_to_update.iter().for_each(|cargo_to_move| {
-        stacks[instructions[2] - 1].push(*cargo_to_move);
+        stacks[instruction.to - 1].push(*cargo_to_move);
     });
 
     stacks
 }
 
-fn parse_instruction(instruction: &str) -> Vec<usize> {
-    instruction
-        .split_whitespace()
-        .filter_map(|s| s.parse::<usize>().ok())
-        .collect()
-}
-
-fn normalize<'a>(input: &'a str) -> String {
+fn normalize(input: &str) -> String {
     input
         .replace(|c: char| !c.is_whitespace() && !c.is_alphanumeric(), "")
         .replace("  ", " ")
